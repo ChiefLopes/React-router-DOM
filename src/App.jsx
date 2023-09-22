6a5c5e44ee803e9adc47aad9bc485003e6e439f6
ChiefLopes <chieflopesekede@gmail.com>
2023-09-22 15:54:00 +0100

    added routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
           <div>
-              <p>para</p>
+              <Router>
+                  <Routes>
+                      <Route />
+                      
+                  </Routes>
+              </Router>
       </div>
     </>
   );
